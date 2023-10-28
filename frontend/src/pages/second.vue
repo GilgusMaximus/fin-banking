a590465cb7894a8673a3a50e316ef344dc983701
Luca Hohmann <hohmann.luca@tutanota.com>
2023-10-28 20:36:08 +0000

    Correct frontend, backend building, frontend containerization, inter-container connection:
    Added:
    - Correct frontend -> Removed Nuxt2 project and initiated a new Nuxt 3 project
    - Frontend containerization -> Containerized Nuxt 3 project
    - Inter-container connection -> Created a custom network over which frontend and backend container can communicate
    
    Fixed:
    - Backend building -> backend is now building correctly, as app.js was not .tx file and removed views folder as no views will be rendered from backend

---
+++ frontend/src/pages/second.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    const { data: count } = await useFetch('http://backend:8080/')
+    console.log("DATAT " + count);
+</script>
+<template>
+    <section>
+      <p>This page will be displayed at the /second route.</p>
+      <p>Page visits: {{ count }}</p>
+    </section>
+    
+</template>
